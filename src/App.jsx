d82c466d988d7216ef762c7a6b20fa5720227c0a
carlos.aquino <carlos.aquino@coppel.com>
2024-08-29 19:28:32 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import CharacterCounter from './component/CharacterCounter'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+       <CharacterCounter />
+    </>
+  )
+}
+
+export default App
